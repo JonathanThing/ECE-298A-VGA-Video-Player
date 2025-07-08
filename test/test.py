bb3603d838b47eb08563d8836e3b08852024063e
JonathanThing <jonathancai834@gmail.com>
2025-07-08 19:08:46 +0000

    Update QSPI testing

--- test/test.py
+++ test/test.py
@@ -3,8 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer
 
 @cocotb.test()
 async def test_project(dut):
@@ -28,35 +27,33 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     dut._log.info("Awaiting CS Low")
-    while dut.uio_out.value & (1 << 2) != 0:
-        await ClockCycles(dut.clk, 1)
+    while dut.uio_out[2] != 0:
+        await FallingEdge(dut.clk)
 
     instruction = 0x6b
     dut._log.info("Sending QSPI Instruction")
 
     for i in range(8):
-        dataOutput = dut.uio_out.value & (1 << 3)
-        print(f"Bit {i} val: {instruction & (1 << (7 - i))} out: {dataOutput}")
-
-        if (instruction & (1 << (7 - i))): # 1
-            assert dataOutput > 0, f"Expected bit {i} to be 1, got {dataOutput}"
+        dataOutput = dut.uio_out[3].value
+        if (instruction & (1 << (7-i))): # 1
+            assert dataOutput == 1, f"Expected bit {i} to be 1, got {dataOutput}"
         else:  # 0
             assert dataOutput == 0, f"Expected bit {i} to be 0, got {dataOutput}"
-        await ClockCycles(dut.clk, 1)
+        await FallingEdge(dut.clk)
 
-    dut._log.info("Instruction sent successfully, Sending 32 bits of dummy data")
+    dut._log.info("Instruction code send successfully, Sending dummy data")
 
     for i in range(32):
         # Check if hold pin is held high
-        outputEnable = dut.uio_oe.value & (1 << 7)
-        dataOutput = dut.uio_out.value & (1 << 7)
-        assert outputEnable > 0, f"Expected output enable to be high at bit {i}, got {outputEnable}"
-        assert dataOutput > 0, f"Expected hold pin to be high at bit {i}, got {dataOutput}"
-        await ClockCycles(dut.clk, 1)
-        
-    outputEnable = dut.uio_oe.value & (1 << 7)  
-    # assert outputEnable == 0, f"Expected output enable to be low at end of instruction, got {outputEnable}"
+        outputEnable = dut.uio_oe[7].value
+        dataOutput = dut.uio_out[7].value
+        assert outputEnable == 1, f"Expected output enable to be high at bit {i}, got {outputEnable}"
+        assert dataOutput == 1, f"Expected hold pin to be high at bit {i}, got {dataOutput}"
+        await FallingEdge(dut.clk)
 
-    await ClockCycles(dut.clk, 20)
+    outputEnable = dut.uio_oe[7].value
+    assert outputEnable == 0, f"Expected output enable to be low at end of instruction, got {outputEnable}"
+    print("QSPI instruction sent successfully")
 
+    await ClockCycles(dut.clk, 5)
     assert True
